b2d7b5ddf668b64d64dc1002ab56d5be6a122f62
aminos <amriamine@protonmail.com>
2018-09-27 20:12:37 +0100

    a function to create geometric law experiences

---
+++ geometric_law/geometric.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# !/bin/env python
+"""
+    A program to illustrate the geometric law of probability
+"""
+
+import random
+import sys
+
+
+def test(p):
+    """
+        Returns 'S' if the random value is <= p
+            else 'E'
+    """
+
+    r = random.random()
+    if r <= p:
+        return 'S'
+    else:
+        return 'E'
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print('Usage: ./geometric.py p')
+        exit(1)
+
+    try:
+        p = float(sys.argv[1])
+    except ValueError:
+        print("No valid float was supplied.")
+        exit(2)
+
+    print("Working with: p = {:f}".format(p))
+    print("Result: {:.1s}".format(test(p)))
+
+    exit(0)
