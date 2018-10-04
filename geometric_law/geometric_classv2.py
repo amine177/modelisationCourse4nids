826620eef095d4a571874a1b7ab3ffab80a505ad
aminos <amriamine@protonmail.com>
2018-10-04 11:12:28 +0100

    added mean calculation

--- geometric_law/geometric_classv2.py
+++ geometric_law/geometric_classv2.py
@@ -7,8 +7,19 @@ from random import random as r
 
 epreuve = lambda p: r() <= p
 
-x = 1
-while not epreuve(float(sys.argv[1])):
-    x += 1
 
-print(x)
+def geom(p):
+    x = 1
+    while not epreuve(float(sys.argv[1])):
+        x += 1
+
+    return x
+
+
+p = float(sys.argv[1])
+rep = 100
+s = 0
+for i in range(rep):
+    s += geom(p)
+
+print(s / rep)
