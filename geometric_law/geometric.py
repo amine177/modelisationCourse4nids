779f62b689696efe814e4ce2c45dac12d3cb6651
aminos <amriamine@protonmail.com>
2018-10-04 11:12:50 +0100

    added mean calculation

--- geometric_law/geometric.py
+++ geometric_law/geometric.py
@@ -18,18 +18,32 @@ def test(p):
     return r <= p
 
 
+def geom(p):
+
+    x = 1
+    while not test(p):
+        x += 1
+
+    return x
+
+
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print('Usage: ./geometric.py p')
+    if len(sys.argv) != 3:
+        print('Usage: ./geometric.py p repeat')
         exit(1)
 
     try:
         p = float(sys.argv[1])
+        rep = int(sys.argv[2])
+
     except ValueError:
         print("No valid float was supplied.")
         exit(2)
 
-    print("Working with: p = {:f}".format(p))
-    print("Result: {:.1s}".format(test(p)))
+    s = 0
+    for i in range(rep):
+        s += geom(p)
 
+    print("The mean of the success rate is {:f}"
+          .format(s / rep))
     exit(0)
