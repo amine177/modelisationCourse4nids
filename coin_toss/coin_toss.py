02c1fac0e27b915f39a32548c8fa942a86709a3e
aminos <amriamine@protonmail.com>
2018-10-06 14:41:57 +0100

    added histogram drawing

--- coin_toss/coin_toss.py
+++ coin_toss/coin_toss.py
@@ -1,7 +1,11 @@
 # !/bin/env python
 # -*- coding: utf-8 -*-
 
+# A script to plot a histogram of n tossess
+
 import random
+import matplotlib.pyplot as plt
+import itertools
 
 
 def toss(p=0.5):
@@ -29,8 +33,48 @@ def ntoss(n, p=0.5):
     return {'H': hs / n, 'T': (n-hs) / n}
 
 
+def plotHistogram(p, ti="X historgram", cl='b', la="Probability", n=3):
+    """
+        plotHistogram(p, ti, cl, la, n) plot a histogram of\n
+        the random variable X : n H/T combinations -> P(X = x)\n
+        p : probability of Head\n
+        ti: title\n
+        cl: color\n
+        la: label\n
+        n : number of tries
+    """
+
+    combinations = [''.join(i) for i in itertools.product('HF', repeat=n)]
+    y = []
+    x = []
+    hp = p
+    tp = 1 - p
+    for i in combinations:
+        hc = i.count('H')
+        if hc == 0:
+            tc = len(i) - hc
+            tot = tp ** tc
+        elif hc < n:
+            tc = len(i) - hc
+            tot = hp ** hc * tp ** tc
+        else:
+            tot = hp ** hc
+
+        if hc in x:
+            y[x.index(hc)] += tot
+        else:
+            y.append(tot)
+            x.append(hc)
+
+    plt.scatter(x, y, label=la, color=cl)
+    plt.title(ti)
+    plt.xticks(x)
+    plt.show()
+
+
 if __name__ == "__main__":
     print("A single toss: {:s}".format(toss()))
     pd = ntoss(1000000)
     print("Porbabilites H: {:f}, T: {:f}"
           .format(pd['H'], pd['T']))
+    plotHistogram(pd['H'], n=25,)
