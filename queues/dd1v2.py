7439d6e77a98d9af7259c25d0c1aba0a1eb9365a
aminos <amriamine@protonmail.com>
2018-10-16 22:13:57 +0100

    added nice messages

--- queues/dd1v2.py
+++ queues/dd1v2.py
@@ -19,7 +19,7 @@ class Customer(object):
         if self.departure_time is None:
             return None
         else:
-            return self.departue_time - self.arrival_time
+            return self.departure_time - self.arrival_time
 
     def __str__(self):
         return "Customer({}, {})".format(self.cid, self.arrival_time)
@@ -28,11 +28,12 @@ class Customer(object):
         return str(self)
 
 
-def simulate_md1(lambd, mu, max_time, verbosity=0):
+def simulate_md1(lambd, mu, max_time, maxcapacity=0, verbosity=0):
+    wait = 0
     served_customers = []
     unserved_customers = []
 
-    md1 = queue.LifoQueue()
+    md1 = queue.Queue(maxcapacity)
 
     next_arrival = 1/lambd
     next_service = next_arrival + 1/mu
@@ -43,16 +44,19 @@ def simulate_md1(lambd, mu, max_time, verbosity=0):
 
         if t == next_arrival:
             c = Customer(arrival_time=t)
-            md1.put(c)
+            if not md1.full():
+                md1.put(c)
 
-            if verbosity >= 2:
-                print("{:10.2f}: Customer {} arrives".format(t, c.cid))
-
+                if verbosity >= 2:
+                    print("{:10.2f}: Customer {} arrives".format(t, c.cid))
+            else:
+                print("Waiting room is full!")
             next_arrival = t + 1/lambd
 
         if t == next_service:
             done_customer = md1.get()
             done_customer.departure_time = t
+            wait += done_customer.wait
             served_customers.append(done_customer)
 
             if verbosity >= 2:
@@ -65,15 +69,16 @@ def simulate_md1(lambd, mu, max_time, verbosity=0):
                 next_service = t + 1/mu
 
             if verbosity >= 1:
-                print("{:10.2f}: {}".format(t, "#"*md1.qsize()))
+                print("{:10.2f}: waiting room: {}".format(t, "#"*md1.qsize()))
 
         t = min(next_arrival, next_service)
 
     while not md1.empty():
         unserved_customers.append(md1.get())
 
-    return served_customers, unserved_customers
+    return served_customers, unserved_customers, wait
 
 
 if __name__ == "__main__":
-    simulate_md1(2, 1, 100, verbosity=2)
+    srv, usrv, wait = simulate_md1(1, 1, 50, maxcapacity=30, verbosity=2)
+    print("Mean of waiting is: {:10.2f}".format(wait/len(srv)))
